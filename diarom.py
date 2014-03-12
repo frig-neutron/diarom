8ab069bf89224e677316b02416468cafa41d74d2
Daniil Barklon <dbarklon@proship.com>
2014-03-12 00:29:13 -0400

    restrict dia import scope + pass api data object, instead of brutishly acquiring it

--- diarom.py
+++ diarom.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import dia
 
 class ROMParseResult:
   words=[]
@@ -41,13 +40,14 @@ def rom_parse_words(words):
   ret.indexedInput = ctx.wordsIndexed
   return ret
 
-def rom_object_gen(string):
+def rom_object_gen(data, string):
+  import dia
   boxType=dia.get_object_type("Flowchart - Box")
-  layer=dia.diagrams()[0].data.active_layer
+  layer=data.active_layer
 
   parsed=rom_parse_words(string)
   for word in reversed(parsed.words):
-    box, h1, h1=boxType.create(0,0)
+    box, h1, h2=boxType.create(0,0)
     box.properties['text'] = word
     layer.add_object(box)
 
@@ -55,20 +55,21 @@ def rom_object_gen(string):
   dia.active_display().flush()
 
 def rom_object_gen_run(data, flags):
-  rom_object_gen("""To make use of a business opportunity, the designer will design a house. 
+  rom_object_gen(data, """Develop a system to aid with recruiting, organizing and retaining cruise ship personnel. 
+  The system should support categorization of opening and candidates using tags. Tags should be reusable and specific. Users should be able to update tags.
+  The system should be accessible over the web on mobile devices. 
+  The system should use the clients' preferred flavour of industry terminology.
+  The system should support secure interaction with non-client personnel, including candidates and MDs. Some functions of system require that the user logs in with an account, other functions should be openly available.
+  MDs should be able to input periodical onboard ratings directly on the web. This is to replace to the current system where MDs submit periodical onboard ratings via PDF or email, requiring manual data-entry.
+  The system should support publishing openings on the web. Candidates accessing the system with a user account should be able to request assignments by sending Musician Contract Management Forms (MCMF). Candidates should be able to submit promotional packages through the system. Registered candidates should have access to a 'refer-a-friend' function.
+  The client should be able to notify candidates of upcoming assignments through the system. 
+  The system should support rich candidate profiles. Candidate profiles can contain links to web materials such as HeadHunter PromoPacks,  EPKs, and YouTube videos, periodical onboard MD ratings, and whether the candidate has an agent or not. The candidate profile should aid the client in tracking the candidates' position in the staffing process, from application on. Example statuses include 'applied' and 'evaluated'. Candidate profiles should include contracting-related attributes.
+  The system should support staffing status reports. Staffing reports should enable the customer to determine current and future staffing on-board each ship. Staffing rosters should be partitionable by agency responsible for employee. The reports should provide information on amount paid to out to each agency.
+  The system may import basic personnel information from various data sources, such as the 'Crew Manning System', which is a Human Resource tool at Carnival, and an in-house database colloquially referred to as 'the Access database'.
+  """)
 
-The house can fly by moving using the air as the primary means of support. The house flies between flat ground areas within a volume with sufficient dimensions to accomodate the house and the physical effects of flying the house and having no objects within it or the space required for moving the house in or out of the area.
-
-The house should be simple to fly by entering and executing a flight plan, abstract of unnecessary technical details. Entering and executing a flight plan is done by using a console-based user interface to enter a specification of a flight destination, intended air route, time of departure and arrival, and instructing the flight computer to execute.
-
-The house takes off and lands vertically. At landing the house will ensure the ground site is clear, stop horizontal motion over the ground site, and slowly reduce altitude until contact with ground. At takeoff the house will ensure the exit route is clear and slowly increase altitude while keeping horizontal motion to zero.
-
-The house will have a 200 km range.
-
-The house will fly to effect ground change of scenery and nearby resources for the occupants. The provides shelter and comfort and facilitates recreation. The house provides shelter and comfort by acting as a physical barrier between the natural environtment and the occupants into an isolated internal environtment, and by granting access to food reserves, sanitation facilities, comfortable furniture, internal atmospheric temperature and pressure control.""")
-
-
-dia.register_action ("DialogsGroupproperties", "ROM Object Generate", "/DisplayMenu/Dialogs/DialogsExtensionStart", rom_object_gen_run)
-
-
-
+if __name__ == '__main__':
+  pass
+else:
+  import dia
+  dia.register_action ("DialogsGroupproperties", "ROM Object Generate", "/DisplayMenu/Dialogs/DialogsExtensionStart", rom_object_gen_run)
