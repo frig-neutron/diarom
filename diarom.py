e2b5773d8b43174f7d68fda74c11d946c5f7b752
Daniil Barklon <dbarklon@proship.com>
2014-03-14 11:36:56 -0400

    ROM object class

--- diarom.py
+++ diarom.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 
+class ROMObject(object): 
+  def __init__(self, text, oid=None):
+
+    openIndex=text.find('[')
+    closeIndex=text.find(']')
+
+    def parseOid(): return int(text[openIndex+1:closeIndex])
+    def stripOid(): return text[0:openIndex].strip()
+    
+    if not oid: 
+      oid = parseOid()
+      text = stripOid()
+
+    self.oid = oid
+    self.text = text.strip()
+
+  def __repr__(self): return self.text+' ['+str(self.oid)+']'
+
+
 class ROMParseResult:
   words=[]
   indexedInput=''
---
+++ diaromtest.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import unittest
+import diarom
+
+def rel(oid, rel=[]): return romgenseq.ObjectRel(oid, rel)
+def oids(rels): return map(lambda r: r.oid, rels)
+
+class DiaRomTest(unittest.TestCase):
+
+  def test_romObjectParse(self):
+    text='voorwerp [42]'
+    obj=diarom.ROMObject(text)
+
+    self.assertEqual(42, obj.oid)
+    self.assertEqual('voorwerp', obj.text)
+
+  def test_romObjectRepr(self):
+    obj=diarom.ROMObject('voorwerp', 42)
+    repr=str(obj)
+    self.assertEqual('voorwerp [42]', repr)
+
+if __name__ == '__main__':
+  unittest.main()
+
